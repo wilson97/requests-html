0a2138b0b4c0d58021a7ad0a8c6df910d0ce819e
wilson97 <wilson97@gmail.com>
2023-05-31 18:55:20 -0700

    sdf

--- requests_html.py
+++ requests_html.py
@@ -781,8 +781,7 @@ class BaseSession(requests.Session):
     @property
     async def browser(self):
         if not hasattr(self, "_browser"):
-            print("HI WILSON'S CODE")
-            self._browser = await pyppeteer.launch(ignoreHTTPSErrors=not(self.verify), headless=False, args=self.__browser_args)
+            self._browser = await pyppeteer.launch(ignoreHTTPSErrors=not(self.verify), headless=True, args=self.__browser_args)
 
         return self._browser
 
